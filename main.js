1fe4ac5d6fda58a6bda43ae408cfb09103c730b5
Ethan Isola <isolaethan@gmail.com>
2022-01-20 08:14:49 -0600

    commit tree e0c55fb234e2b815f94254dd36e3300748f36b55

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
+const init = () => {
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
+const inputs = document.body.getElementsByClassName('like');
+function addingEventListener(input){
+  input.addEventListener('click', function() {
+    mimicServerCall()
+    .then(function (object) {
+      if(object.textContent != "Random server error. Try again."){
+        let counter = 0;
+        if (input.textContent == `Like! ${EMPTY_HEART}`){
+          input.classList.add("activated-heart")
+          input.textContent = `Like! ${FULL_HEART}`;
+          counter++;
+        }
+        else{
+          input.textContent = `Like! ${EMPTY_HEART}`;
+          counter++;
+          input.classList.remove("activated-heart")
+        }
+      }
+      console.log(object);
+    })
+    .catch(function (error) {
+      let err = document.getElementById('modal')
+      err.classList.remove("hidden")
+      setTimeout(() => {
+        err.classList.add("hidden")
+      }, 3000)
+      err.innerHTML = `${error}`
+      console.log(err)
+    })
+  })
+}
+for (var i = 0 ; i < inputs.length; i++) {
+  addingEventListener(inputs[i])
+}
+}
 
 
 //------------------------------------------------------------------------------
@@ -23,3 +57,7 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+
+
+document.addEventListener('DOMContentLoaded', init);
